7a0492b60c6b15bacea39e240eff6c7c039eaa9a
Antonia Rohne <antonia.rohne@fluffyfairygames.com>
2018-09-20 00:02:27 +0200

    added pushnotification started unlocking system *unversioned files

---
+++ Assets/Scripts/App/ListObject.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace App
+{
+	[CreateAssetMenu(menuName = "ListObject")]
+	public class ListObject<T> : ScriptableObject
+	{
+		public T[] List;
+
+		public T[] GetList()
+		{
+			return List;
+		}
+	}
+}
