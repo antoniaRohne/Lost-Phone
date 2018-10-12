20213c39891aec42320a071f0c3129353f9ecc6c
Antonia Rohne <antonia.rohne@fluffyfairygames.com>
2018-10-12 10:47:35 +0200

    - forgot files

---
+++ Assets/Scripts/Contact/MessageView.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MessageView : MonoBehaviour
+{
+
+	[SerializeField] public Text MessageText;
+	[SerializeField] private HorizontalLayoutGroup _messageContent;
+
+	public void setAlignmentToRight()
+	{
+		_messageContent.childAlignment = TextAnchor.UpperRight;
+		MessageText.alignment = TextAnchor.UpperRight;
+	}
+	
+}
---
+++ Assets/Scripts/PrefabList.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "PrefabListObject")]
+public class PrefabList : ScriptableObject
+{
+
+    public AppView AppPrefab;
+    public PushNotificationConfig EventPrefab;
+    public PasswordPanelView PasswordPanelPrefab;
+
+}
